c5e4f7ea1ee0ddf3847c34219fa1e0928e5e4feb
JunUeda <ueue.jon@gmail.com>
2026-04-02 22:53:19 +0900

    キャッチフレーズの改行とモバイル表示の最適化

--- src/components/Concept.jsx
+++ src/components/Concept.jsx
@@ -4,7 +4,10 @@ import './Concept.css';
 const Concept = () => {
   return (
     <section className="concept-section container">
-      <p className="concept-text">同級生と、もっと深く繋がりたい！を叶える。そんなサイトを作りました</p>
+      <p className="concept-text">
+        <span className="concept-line">同級生と、もっと深く繋がりたい！を叶える。</span>
+        <span className="concept-line">そんなサイトを作りました</span>
+      </p>
     </section>
   );
 };
